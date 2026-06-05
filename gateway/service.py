ff72737be59ad7006f2233a60a6415f6b46b7b59
euginiagabrielle <icin2004@gmail.com>
2026-06-06 00:12:30 +0700

    add gateway unit akademik

---
+++ gateway/service.py
@@ -0,0 +1,47 @@
+import json
+from nameko.web.handlers import http
+from nameko.rpc import RpcProxy
+from werkzeug.wrappers import Response
+
+class GatewayService:
+    name = "gateway"
+
+    master_service = RpcProxy("master_service")
+
+    @http('POST', '/master/units')
+    def create_unit(self, request):
+        payload = json.loads(request.get_data(as_text=True))
+
+        result = self.master_service.create_unit(
+            name=payload.get('name'),
+            type=payload.get('type'),
+            parent_id=payload.get('parent_id')
+        )
+        return Response(json.dumps(result), mimetype='application/json')
+    
+    @http('GET', '/master/units')
+    def get_all_units(self, request):
+        result = self.master_service.get_all_units()
+        return Response(json.dumps(result), mimetype='application/json')
+    
+    @http('GET', '/master/units/<string:unit_id>')
+    def get_unit_by_id(self, request, unit_id):
+        result = self.master_service.get_unit_by_id(unit_id)
+        return Response(json.dumps(result), mimetype='application/json')
+    
+    @http('PUT', '/master/units/<string:unit_id>')
+    def update_unit(self, request, unit_id):
+        payload = json.loads(request.get_data(as_text=True))
+
+        result = self.master_service.update_unit(
+            unit_id=unit_id,
+            name=payload.get('name'),
+            type=payload.get('type'),
+            parent_id=payload.get('parent_id')
+        )
+        return Response(json.dumps(result), mimetype='application/json')
+    
+    @http('DELETE', '/master/units/<string:unit_id>')
+    def delete_unit(self, request, unit_id):
+        result = self.master_service.delete_unit(unit_id)
+        return Response(json.dumps(result), mimetype='/application/json')
