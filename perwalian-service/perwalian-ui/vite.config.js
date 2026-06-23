efa72a118e3ea3b303ff0752a1729d8f608e71e5
ValentinoEzinkyJoelianto <c14230138@john.petra.ac.id>
2026-06-23 19:26:08 +0700

    Berhasil Panggil semua database ke UI (tanpa PRS)

--- perwalian-service/perwalian-ui/vite.config.js
+++ perwalian-service/perwalian-ui/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Backend URL - ganti sesuai dengan BitVise SSHnya
+// Backend URL - ganti sesuai dengan BitVise SSHnya.
 // Untuk Docker lokal: http://localhost:8003
 // Untuk AWS EC2:      http://13.220.219.2:8003
 const BACKEND_URL = 'http://100.49.43.222:8003';
